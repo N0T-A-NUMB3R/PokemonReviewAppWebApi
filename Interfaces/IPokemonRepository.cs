9910384de1a83d750f6c573eb2fb02f71a1a1f5b
Fabio NAN <fabio.nan@finsa.it>
2024-01-21 09:21:17 +0100

    Aggiungere i file di progetto.

---
+++ Interfaces/IPokemonRepository.cs
@@ -0,0 +1,16 @@
+﻿using PokemonReviewApp.Models;
+using System.Globalization;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface IPokemonRepository
+    {
+        ICollection<Pokemon> GetPokemons();
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(String Name);
+
+        int GetPokemonRating(int pokemonId);
+
+        bool PokemonExists(int id);
+    }
+}
---
+++ Repository/PokemonRepository.cs
@@ -0,0 +1,47 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly DataContext _Context;
+        public PokemonRepository(DataContext context) 
+        {
+            _Context = context;
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _Context.Pokemons.OrderBy(p => p.Id).ToList();
+        }
+
+        Pokemon IPokemonRepository.GetPokemon(int PokeId)
+        {
+            return _Context.Pokemons.Where(p => p.Id == PokeId).SingleOrDefault();
+        }
+          
+        Pokemon IPokemonRepository.GetPokemon(string Name)
+        {
+            return _Context.Pokemons.Where(p => p.Name == Name).FirstOrDefault();
+        }
+
+        int IPokemonRepository.GetPokemonRating(int pokemonId)
+        {
+            var review = _Context.Reviews.Where(p => p.Id == pokemonId);
+            if (review.Count() == 0)
+            {
+                return 0;
+            }
+            return review.Sum(r => r.Rating) / review.Count();
+        }
+
+       
+
+        bool IPokemonRepository.PokemonExists(int id)
+        {
+            return _Context.Pokemons.Any(p => p.Id == id);
+        }
+    }
+}
