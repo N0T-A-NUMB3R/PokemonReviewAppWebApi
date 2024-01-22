913080efc4272c006e917d5be58be5b0434763e5
Fabio NAN <fabio.nan@finsa.it>
2024-01-22 14:23:11 +0100

    Aggiunto Owner

---
+++ Controllers/OwnerController.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using PokemonReviewApp.Repository;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwnerController : ControllerBase
+    {
+        private readonly IOwnerRepository _ownerRepository;
+        private readonly IMapper _mapper;
+
+        public OwnerController(IOwnerRepository ownerRepository, IMapper mapper)
+        {
+            _ownerRepository = ownerRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Owner>))]
+        public IActionResult GetOwners()
+        {
+            var owners = _mapper.Map<List<OwnerDto>>(_ownerRepository.GetOwners());
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(owners);
+        }
+
+        // GET api/<OwnerController>/5
+        [HttpGet("{ownerId}")]
+        [ProducesResponseType(200, Type = typeof(Owner))]
+        [ProducesResponseType(400)]
+        public IActionResult GetOwner(int ownerId)
+        {
+            if (!_ownerRepository.OwnerExists(ownerId))
+            {
+                return NotFound();
+            }
+
+            var owner = _mapper.Map<CategoryDto>(_ownerRepository.GetOwner(ownerId));
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(owner);
+        }
+
+        [HttpGet("{ownerId}/pokemons")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Owner>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonsByOwner(int ownerId)
+        {
+            if (!_ownerRepository.OwnerExists(ownerId))
+            {
+                return NotFound();
+            }
+
+            var owner = _mapper.Map<List<PokemonDto>>(
+                _ownerRepository.GetPokemonByOwner(ownerId));
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(owner);
+        }
+
+    }
+}
+}
---
+++ Repository/OwnerRepository.cs
@@ -0,0 +1,41 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        private readonly DataContext _dataContext;
+
+        public OwnerRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        Owner IOwnerRepository.GetOwner(int ownerId)
+        {
+            return _dataContext.Owners.SingleOrDefault(o => o.Id == id);        }
+
+        ICollection<Owner> IOwnerRepository.GetOwnerOfAPokemon(int pokeId)
+        {
+            return _dataContext.PokemonOwners.Where(p => p.PokemonId == pokeId).Select(o => o.Owner).ToList();
+        }
+
+        ICollection<Owner> IOwnerRepository.GetOwners()
+        {
+            return _dataContext.Owners.ToList();
+        }
+
+        ICollection<Pokemon> IOwnerRepository.GetPokemonByOwner(int ownerId)
+        {
+            return _dataContext.PokemonOwners.Where(p => p.OwnerId == ownerId).Select(p => p.Pokemon).ToList();
+
+        }
+
+        bool IOwnerRepository.OwnerExists(int ownerId)
+        {
+            return _dataContext.Owners.Any(o => o.Id == ownerId);
+        }
+    }
+}
